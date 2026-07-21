af041fa0c3f592607b6eab6ba53ba92abc3e746a
7PREV <you@example.com>
2026-07-21 06:53:45 +0000

    Hotfix: force-disable stale launch lock cache

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -3,6 +3,20 @@ import ReactDOM from "react-dom/client";
 import "@/index.css";
 import App from "@/App";
 
+if (typeof window !== "undefined") {
+  if ("serviceWorker" in navigator) {
+    navigator.serviceWorker.getRegistrations()
+      .then((registrations) => registrations.forEach((registration) => registration.unregister()))
+      .catch(() => {});
+  }
+
+  if ("caches" in window) {
+    window.caches.keys()
+      .then((keys) => Promise.all(keys.map((key) => window.caches.delete(key))))
+      .catch(() => {});
+  }
+}
+
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
